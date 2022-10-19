5de0517896c39095038823b3f06e05b42cd004f1
jpatrickldg <jpatrickldg@gmail.com>
2022-10-19 21:05:27 +0800

    Add design and responsiveness

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,14 @@
 class HomeController < ApplicationController
   def index
     if user_signed_in?
-      @categories = current_user.categories
-      render :dashboard
+      redirect_to dashboard_path
+    else
+      redirect_to new_user_session_path
     end
+  end
+
+  def dashboard
+    @q = current_user.categories.ransack(params[:q])
+    @categories = @q.result(distinct: true)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
-
+  get '/dashboard' => 'home#dashboard'
 end
